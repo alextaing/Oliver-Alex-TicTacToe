2d6cdc6df60e0b6358179e1a0463484abcbc97cc
Alex Taing <ataing@yext.com>
2022-05-19 15:23:05 -0400

    tic-tac-toe

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import './CSS/index.css'; // what is good practice for import css files??
+
+import Game from './components/Game';
+
 function App() {
   return (
-    <h1>TEST</h1>
+    <div>
+      <h1>Oshi v. Alex</h1>
+      <Game />
+    </div>
   );
 }
 
