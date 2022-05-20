80dcf22d848644816537d555124dc91a2f88100c
Alex Taing <ataing@yext.com>
2022-05-20 09:42:46 -0400

    fix formatting and change indent size to 2

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,32 +1,32 @@
-import Square  from "./Square"
+import Square from "./Square"
 
 export default function Board(props: any) {
-    function renderSquare(i: number) {
-        return(
-            <Square 
-                value={props.squares[i]}
-                onClick={() => props.onClick(i)}
-            />
-        );
-    }
-
+  function renderSquare(i: number) {
     return (
-        <div>
-        <div className="board-row">
-          {renderSquare(0)}
-          {renderSquare(1)}
-          {renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {renderSquare(3)}
-          {renderSquare(4)}
-          {renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {renderSquare(6)}
-          {renderSquare(7)}
-          {renderSquare(8)}
-        </div>
-      </div>
+      <Square
+        value={props.squares[i]}
+        onClick={() => props.onClick(i)}
+      />
     );
+  }
+
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
 }
